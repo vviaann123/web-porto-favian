080988544f293dbeff8a7b99f53b33f9496fa2cc
vviaann123 <andhikafavian14@gmail.com>
2025-01-12 16:09:57 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+const closeIcon = document.getElementById("nav-close");
+
+navLink.forEach((link) => (
+    link.addEventListener("click", () => {
+        navMenu.classList.add("hidden")
+    })
+))
+closeIcon.addEventListener("click", () => {
+        navMenu.classList.add("hidden")
+})
+hamburger.addEventListener("click", () => {
+        navMenu.classList.remove("hidden")
+})
+
+
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+if(localStorage.getItem("mode") == "dark") {
+    darkMode();
+} else {
+    lightMode();
+}
+
+themeBtn.addEventListener("click", (e) => {
+    if(localStorage.getItem("mode") == "light") {
+        darkMode();
+    } else {
+        lightMode();
+    }
+})
+
+function darkMode() {
+    html.classList.add("dark");
+    themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+    localStorage.setItem("mode", "dark");
+}
+function lightMode() {
+    html.classList.remove("dark");
+    themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+    localStorage.setItem("mode", "light");
+}
+
